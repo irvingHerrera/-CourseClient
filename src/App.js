7d30bd3ad391154e95bbc9d59cd24ad5ce73b139
Irving Herrera <irving.herreramolina@gmail.com>
2020-05-12 20:21:45 -0500

    init proyect, add sass and ant desing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker } from 'antd';
+import './App.scss';
 
 function App() {
+
+  const test = (date, dateString) => {
+    console.log(date);
+    console.log(dateString);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <h1>Web Persona - Client</h1>
+     <h2>Proyecto</h2>
+     <DatePicker onChange={test} />
     </div>
   );
 }
