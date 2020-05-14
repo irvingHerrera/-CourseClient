5b08396fdcfbbfed1a194b604ca19c4a018875a2
Irving Herrera <irving.herreramolina@gmail.com>
2020-05-14 18:02:35 -0500

    estructura layouts

---
+++ src/layouts/LayoutBasic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+export default function() {
+    return(
+        <Layout>
+            <h2>Menu Sider...</h2>
+            <div>Content...</div>
+            <h2>Footer...</h2>
+        </Layout>
+    );
+}
