d51c09f53904720222f6a553cc75e8ed037a6eb1
Irving Herrera <irving.herreramolina@gmail.com>
2020-05-15 01:31:06 -0500

    creacion de fichero de rutas

---
+++ src/config/router.js
@@ -0,0 +1,28 @@
+// Layaout
+import LayoutAdmin from '../layouts/LayoutAdmin';
+
+// Admin Pages
+import AdminHome from '../pages/Admin';
+import AdminSingIn from '../pages/Admin/SingIn';
+
+const routes = [
+    {
+        path: '/admin',
+        component: LayoutAdmin,
+        exact: false,
+        routers: [
+            {
+                path: '/admin',
+                component: AdminHome,
+                exact: true
+            },
+            {
+                path: '/admin/login',
+                component: AdminSingIn,
+                exact: true
+            }
+        ]
+    }
+];
+
+export default routes;
