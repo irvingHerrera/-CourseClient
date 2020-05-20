3e2fe98de1276e39d65e93faa0ab97e89b0fe779
Irving Herrera <irving.herreramolina@gmail.com>
2020-05-20 00:04:32 -0500

    add menu sider

---
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Icon } from 'antd';
+
+import './MenuSider.scss';
+
+export default function MenuSider() {
+
+    const { Sider } = Layout;
+
+    return (
+        <Sider className='admin-sider'>
+        <Menu theme='dark' mode='inline' defaultOpenKeys={['1']}>
+            <Menu.Item key='1'>
+                <Link to={'/admin'}>
+                    <Icon type='home'></Icon>
+                    <span className='nav-text'>Home</span>
+                </Link>
+            </Menu.Item>
+            <Menu.Item key='2'>
+                <Link to={'/admin/menu-web'}>
+                    <Icon type='menu'></Icon>
+                    <span className='nav-text'>Menu web</span>
+                </Link>
+            </Menu.Item>
+        </Menu>
+        </Sider>
+    );
+}
