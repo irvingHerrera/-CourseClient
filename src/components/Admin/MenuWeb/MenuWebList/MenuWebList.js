c2d5e49516cf265d2cbfc3af2d83e4bd47c1dee7
Irving Herrera <irving.herreramolina@gmail.com>
2020-07-15 12:32:13 -0500

    creacion de menu web list

---
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, List, Button, Icon, Modal as ModalAntd, notification } from 'antd';
+import Modal from '../../../Modal';
+import DragSortableList from 'react-drag-sortable'
+
+import './MenuWebList.scss'
+
+const { confirm } = ModalAntd;
+
+export default function MenuWebList(props) {
+    const { menu, setReloadMenu } = props;
+    const [listItems, setlistItems] = useState([]);
+    const [isVisibleModal, setVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState('');
+    const [modalContent, setModalContent] = useState(null);
+
+    useEffect(() => {
+        const listItemsArray = [];
+
+        menu.forEach(item => {
+            console.log(item);
+            listItemsArray.push({
+                content: (<MenuItem item={item}></MenuItem>)
+            });
+        })
+        setlistItems(listItemsArray);
+    }, [menu])
+
+    const onSort = (sortedList, dropEvent) => {
+        console.log(sortedList);
+    };
+
+    return(
+        <div className='menu-web-list'>
+            <div className='menu-web-list__header'>
+                <Button 
+                type='primary'>
+                    Nuevo Menu
+                </Button>
+            </div>
+            <div className='menu-web-list__items'>
+                <DragSortableList
+                 items={listItems}
+                 onSort={onSort}
+                 type='vertical'
+                >
+                </DragSortableList>
+            </div>
+        </div>
+    );
+}
+
+function MenuItem(props) {
+    const { item } = props;
+
+    return (
+        <List.Item
+        actions={[
+            <Switch defaultChecked={item.active}></Switch>,
+            <Button type='primary'>
+                <Icon type='edit'></Icon>
+            </Button>,
+            <Button type='danger'>
+            <Icon type='delete'></Icon>
+        </Button>
+        ]}
+        >
+            <List.Item.Meta title={item.title} description={item.url}></List.Item.Meta>
+        </List.Item>
+    );
+}
--- src/pages/Admin/MenuWeb/MenuWeb.js
+++ src/pages/Admin/MenuWeb/MenuWeb.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { getMenuApi } from '../../../api/menu';
+import MenuWebList from '../../../components/Admin/MenuWeb/MenuWebList';
 
 import './MenuWeb.scss'
 
 export default function MenuWeb() {
 
+    const [menu, setMenu] = useState([]);
+    const [reloadMenu, setReloadMenu] = useState(false);
+
+    useEffect(() => {
+        getMenuApi().then(response => {
+            setMenu(response.menus);
+        });
+
+        setReloadMenu(false);
+    }, [reloadMenu]);
+
     return(
-        <div className='menu-web'><h1>Menu</h1></div>
+        <div className='menu-web'>
+            <MenuWebList menu={menu} setReloadMenu={setReloadMenu}></MenuWebList>
+        </div>
     );
 }
