f8b2720926da312d4a20725342f8636539853ead
Irving Herrera <irving.herreramolina@gmail.com>
2020-08-18 22:56:04 -0500

    obtener datos de curso api udemy

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { getCourseDataUdemyApi } from '../../../../api/courses'
 
 export default function CoursesList(props) {
     const { courses, setReloadCourses } = props;
-
-    console.log(props);
+    if(courses.length > 0) {
+        courses.forEach(element => {
+            getCourseDataUdemyApi(element.idCourse)
+            .then(response => {
+                console.log('sss',response);
+            })
+        });
+    }
 
     return (
         <div>
