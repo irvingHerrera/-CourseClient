54ca9d0ea3d0a8a739119d10a5dd389c936ff914
Irving Herrera <irving.herreramolina@gmail.com>
2020-08-27 13:38:52 -0500

    obteniendo pots

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,13 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import { Button, notification } from 'antd';
+import queryString from 'query-string';
 import Modal from '../../../components/Modal';
+import { withRouter } from 'react-router-dom';
+import {getPostApi} from '../../../api/post';
 
 import './Blog.scss';
 
-export default function Blog() {
+function Blog(props) {
+    const { location, history } = props;
     const [modalTitle, setModalTitle] = useState('');
     const [isVisible, setisVisible] = useState(false);
     const [modalContent, setmodalContent] = useState(null);
+    const [reloadPosts, setReloadPosts] = useState(false)
+    const [post, setPost] = useState(null)
+    const {page = 1} = queryString.parse(location.search);
+
+    console.log(post);
+
+    useEffect(() => {
+        getPostApi(12, page)
+        .then(response => {
+            if(response?.code !== 200) {
+                notification['warning']({
+                    message: response.message
+                });
+            } else {
+                setPost(response.posts);
+            }
+        })
+        .catch(err => {
+            notification['error']({
+                message: 'Error del servidor.'
+            });
+        });
+        setReloadPosts(false);
+    }, [page])
 
     return (
         <div className='blog'>
@@ -26,3 +54,5 @@ export default function Blog() {
         </div>
     )
 }
+
+export default withRouter(Blog);
